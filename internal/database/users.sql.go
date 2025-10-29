cb31981fe057d3d6ff66f94bedbc15b3ff04d7e9
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-29 21:09:04 +0100

    implement resetDB command

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -47,9 +47,17 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 	return i, err
 }
 
+const deleteAllUsers = `-- name: DeleteAllUsers :exec
+DELETE FROM users
+`
+
+func (q *Queries) DeleteAllUsers(ctx context.Context) error {
+	_, err := q.db.ExecContext(ctx, deleteAllUsers)
+	return err
+}
+
 const getUser = `-- name: GetUser :one
-SELECT id, created_at, updated_at, name FROM users
-WHERE name = $1 LIMIT 1
+SELECT id, created_at, updated_at, name FROM users WHERE name = $1
 `
 
 func (q *Queries) GetUser(ctx context.Context, name string) (User, error) {
