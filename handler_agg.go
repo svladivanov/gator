aa4be3ec8598da6fafc7ac7e380bef7bfb19dc41
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-30 08:17:39 +0100

    RSS

---
+++ handler_agg.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerAgg(s *state, cmd command) error {
+	feedURL := "https://www.wagslane.dev/index.xml"
+	feed, err := fetchFeed(context.Background(), feedURL)
+	if err != nil {
+		return fmt.Errorf("couldn't fetch feed: %w", err)
+	}
+	fmt.Printf("Feed: %+v\n", feed)
+
+	return nil
+}
